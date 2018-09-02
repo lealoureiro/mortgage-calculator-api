5cc56e9f8faaa8cd7654e126809002f02b72c5f5
Leandro Loureiro <leandroloureiro@protonmail.com>
2018-09-02 20:42:35 +0200

    removing unnecessary dependency

--- build.sh
+++ build.sh
@@ -2,8 +2,6 @@
 
 set -xe
 
-go get "github.com/julienschmidt/httprouter"
 go get "github.com/gorilla/mux"
-
 
 env GOOS=linux go build -v -ldflags '-d -s -w' -a  -tags netgo -installsuffix netgo -o bin/mortgage-calculator mortgage-calculator.go
