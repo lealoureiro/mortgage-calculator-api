3a25b4fb3db60569daff1da095bc57ee67e74e46
Leandro Manuel (L.M.) Sousa Loureiro <leandro.manuel.sousa.loureiro@ing.com>
2018-09-04 16:56:29 +0200

    starting using dep as dependency manager

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 set -xe
 
-go get "github.com/gorilla/mux"
+echo "Getting project dependencies..."
+dep ensure
 
+echo "Compiling application..."
 env GOOS=linux go build -v -ldflags '-d -s -w' -a  -tags netgo -installsuffix netgo -o bin/mortgage-calculator-api mortgage-calculator-api.go
